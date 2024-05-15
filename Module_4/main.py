8e0a00797832afba64af19d517ede866304bd208
nnigorenko <167545999+nnigorenko@users.noreply.github.com>
2024-05-15 22:53:27 +0300

    Add files via upload

---
+++ Module_4/main.py
@@ -0,0 +1,3 @@
+from mod1 import say_hello as once, say_hello_again as more
+once()
+more()
---
+++ Module_4/mod1.py
@@ -0,0 +1,11 @@
+def say_hello():
+    print('Hello, world!')
+
+
+def say_hello_again():
+    print('Once more: hello, world!!!')
+
+
+if __name__ == '__main__':
+    say_hello()
+    say_hello_again()
