f380af7a6369c326083bd9c4eb228ad6498c5a3a
nnigorenko <167545999+nnigorenko@users.noreply.github.com>
2024-05-18 23:44:53 +0300

    Add files via upload

--- module3hard.py
+++ module3hard.py
@@ -1,7 +1,23 @@
-# Этот код и приведенный в задании ответ не учитывают числа,
-# содержащиеся внутри строк. Если учитывать - ответ будет 101.
-# Планирую написать функцию, которая будет считать сумму чисел,
-# содержащихся внутри произвольных строк. С ходу не получилось:(
+# Добавлена функция numbers_sum_in_string, вычисляющая
+# сумму чисел, содержащихся внутри произвольных строк.
+# В итоге получился правильный ответ - 101.В ответе,
+# приведенном в задании, не учтено число 2 в строке 'Urban2'.
+# Для ответа 99 закомментировать строку 37
+def numbers_sum_in_string(line):
+    numbers_sum = 0
+    sample = ''
+    for i in range(len(line)):
+        if line[i].isdigit():
+            sample += line[i]
+        else:
+            if sample != '':
+                numbers_sum += int(sample)
+                print(sample, numbers_sum)
+            sample = ''
+    if sample != '':
+        numbers_sum += int(sample)
+    return numbers_sum
+
 
 data_structure = [
     [1, 2, 3],
@@ -18,18 +34,16 @@ def calculate_structure_sum(*args):
     for i in range(len(args)):
         if isinstance(args[i], str):
             count += len(args[i])
+            count += numbers_sum_in_string(args[i])  # Если закомментировать, ответ будет 99
         elif isinstance(args[i], int):
             count += args[i]
         elif isinstance(args[i], dict):
             calculate_structure_sum(*args[i].items())
         else:
             calculate_structure_sum(*args[i])
-#     return count
-#
-#
-# result = calculate_structure_sum(data_structure)
-# print(result) так по условиям задания, но при использовании global возврат не нужен
+    return count
 
 
-calculate_structure_sum(data_structure)
-print(count)
+result = calculate_structure_sum(data_structure)
+print(result)
+
